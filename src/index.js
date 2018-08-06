b5cc0bb282a71cfb893dfca66fa7609b9fa0810a
Michael Tauer <michael.tauer@futurice.com>
2018-08-06 12:22:50 +0200

    Disable Redux dev tools as it cased a bug with a too big search tree

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createEpicMiddleware, combineEpics } from 'redux-observable';
 
@@ -10,17 +10,9 @@ import App from './App';
 import './index.css';
 
 const epicMiddleware = createEpicMiddleware();
-const enhancers = [];
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line no-underscore-dangle
-  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
-  if (typeof devToolsExtension === 'function') {
-    enhancers.push(devToolsExtension());
-  }
-}
 const store = createStore(
   monteCarloTreeSearchReducer,
-  compose(applyMiddleware(epicMiddleware), ...enhancers),
+  applyMiddleware(epicMiddleware),
 );
 epicMiddleware.run(combineEpics(monteCarloTreeSearchEpic));
 const target = document.getElementById('root');
