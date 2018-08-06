d33163845579225bbecb6ec187ff65ff0d5cac71
Michael Tauer <michael.tauer@futurice.com>
2018-08-06 12:32:28 +0200

    Return -1 as value if the game result was a draw -> loosing or draws are equally bad

--- src/connectFour.js
+++ src/connectFour.js
@@ -47,7 +47,7 @@ export function getValue(state = initialState, timePenalty = 0) {
   switch (winner) {
     case X: return 1;
     case O: return -1;
-    default: return timePenalty;
+    default: return isEmpty(getValidActions(state)) ? -1 : timePenalty;
   }
 }
 
