fe8bc63188acd1ea630cd9cffe8a2f817d133309
Michael Tauer <michael.tauer@futurice.com>
2018-08-10 10:42:38 +0200

    Add console output for Pandamic preparation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import fromPairs from 'lodash/fromPairs';
 import range from 'lodash/range';
 
+import { initialState } from './pandemic';
 import { getGameState, getSearchTreeRoot, performGameActionAction } from './monteCarloTreeSearchRedux';
 import ConnectFourBoard from './ConnectFourBoard';
 import ConnectFourBoardAnalysis from './ConnectFourBoardAnalysis';
@@ -41,6 +42,7 @@ const App = ({ gameState, searchTreeRoot, onBoardCellClick }) => {
   const nextActionsUcb1Map = fromPairs(
     nextActionNodes.map(n => [n.action.index, n.ucb1]),
   );
+  console.log('Pandemic inital state', initialState);
   return (
     <Container>
       <Title>Monte Carlo Tree Search for Connect 4</Title>
