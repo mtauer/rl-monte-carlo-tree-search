5e42f56022f8fa7f70bec10f6ae5125a4c4f198d
Michael Tauer <michael.tauer@futurice.com>
2018-08-11 23:45:58 +0200

    Temporarily do not run epics

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createEpicMiddleware, combineEpics } from 'redux-observable';
+import { createEpicMiddleware } from 'redux-observable';
 
-import monteCarloTreeSearchReducer, { monteCarloTreeSearchEpic } from './monteCarloTreeSearchRedux';
+import monteCarloTreeSearchReducer from './monteCarloTreeSearchRedux';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import './index.css';
@@ -14,7 +14,8 @@ const store = createStore(
   monteCarloTreeSearchReducer,
   applyMiddleware(epicMiddleware),
 );
-epicMiddleware.run(combineEpics(monteCarloTreeSearchEpic));
+// TODO Uncomment run enable epics
+// epicMiddleware.run(combineEpics(monteCarloTreeSearchEpic));
 const target = document.getElementById('root');
 
 ReactDOM.render(
