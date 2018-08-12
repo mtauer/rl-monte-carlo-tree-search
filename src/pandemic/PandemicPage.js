44eccf459634a137158cc50637e7655f1135e7ce
Michael Tauer <michael.tauer@futurice.com>
2018-08-12 17:56:05 +0200

    Show player positions

--- src/pandemic/PandemicPage.js
+++ src/pandemic/PandemicPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Container, Title } from '../components/Page';
 import NextActionsSection from './NextActionsSection';
+import PlayersSection from './PlayersSection';
 
 const PandemicPage = () => (
   <Container>
     <Title>Monte Carlo Tree Search for Pandemic</Title>
     <NextActionsSection />
+    <PlayersSection />
   </Container>
 );
 
---
+++ src/pandemic/PlayersSection.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import keys from 'lodash/keys';
+
+import { getGameState } from './redux';
+import {
+  Section, SectionTitle, Row, Label,
+} from '../components/Page';
+
+const PlayerContainer = styled.div`
+  flex: 1;
+`;
+
+const PlayersSection = ({ players }) => (
+  <Section>
+    <Row>
+      { players.map(player => (
+        <PlayerContainer key={player.id}>
+          <SectionTitle>Player {player.id}</SectionTitle>
+          <Label>Position: {player.position}</Label>
+        </PlayerContainer>
+      ))}
+    </Row>
+  </Section>
+);
+PlayersSection.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  players: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  const gameState = getGameState(state);
+  const players = keys(gameState.playerPosition).map(id => ({
+    id,
+    position: gameState.playerPosition[id],
+    cards: gameState.playerCards[id],
+  }));
+  return {
+    players,
+  };
+};
+export default connect(mapStateToProps)(PlayersSection);
